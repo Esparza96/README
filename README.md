6ec9a28c3a143c2c35cd3022d50dbfd1f0d3bb45
alex <alexesparza53@gmail.com>
2020-06-18 23:14:42 -0700

    README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# README
+# This CLI app allows the user to creat a README.md file by answering a series of quistions.
+    ## Description
+    Download all the require files.Second by using node.js run npm to get the dependencies needed to run the program.finally, run "node index" to start answering questions revelant to your README file.
+    ## Table of Contents
+    
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[License](#license)
+​
+    ## Installation
+​
+    once you have all the necessary components, run "node index" to start answering questions.
+​
+    ## Usage
+​
+    
+​
+    ## License
+​
+    this project currently has no License.
+​
+    ## Contributing
+​
+    users can email me or open an issue in GITHUB repository.
+
+    ## Test
+
+
+    ## Questions
+​
+    undefined
---
+++ index.js
@@ -0,0 +1,118 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+   return inquirer.prompt([
+        {
+            message: "Welcome to the README.md generator!",
+            name: "welcome",
+            type: "input",
+        },
+        {
+            message: "What is the name of your project?",
+            name: "project",
+            type: "input,"
+        },
+        {
+            message: "Enter a description of your project:",
+            name: "description",
+            type: "input",
+        },
+        {
+            message: "Enter the steps for installing your project:",
+            name: "installation",
+            type: "input",
+        },
+        {
+            message: "How do you use your project? Provide examples",
+            name: "usage",
+            type: "input",
+        },
+        {
+            message: "Does your project have a license? If so, please enter the name of the license",
+            name: "license",
+            type: "input",
+        },
+        {
+            message: "How can users contribute to your project?",
+            name: "contributing",
+            type: "input",
+        },
+        {
+            message: "Enter your Github username:",
+            name: "user-info",
+            type: "input",
+        },
+        {
+            message: "Enter the link to your project:",
+            name: "link",
+            type: "input",
+        },
+        {
+            message: "how would users leave a question?",
+            name: "questions",
+            type: "input",
+        },
+        {
+            message: "does your project need any badges? if so enter at least one.",
+            name: "badges",
+            type: "input",
+
+        },
+    ]);
+}
+
+function generateReadMe(answers) {
+return `# ${answers.project}
+    ## Description
+    ${answers.description}
+    ## Table of Contents
+    
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[License](#license)
+​
+    ## Installation
+​
+    ${answers.installation}
+​
+    ## Usage
+​
+    ${answers.usage}
+​
+    ## License
+​
+    ${answers.license}
+​
+    ## Contributing
+​
+    ${answers.contributing}
+
+    ## Test
+
+
+    ## Questions
+​
+    ${answers.questions}`;
+
+
+
+
+
+}
+
+promptUser()
+    .then(function(answers) {
+        const readme = generateReadMe(answers)
+
+        return writeFileAsync("README.md", readme);
+    })
+    .then(function() {
+        console.log("README generated succesfully!")
+    })
+    .catch(function(err){
+       console.log(err)
+    }
+    );
